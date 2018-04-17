686185649eae06d4d11fea2fc9a8a3316cc516eb
EduardoJAVA <ecm3198@gmail.com>
2018-04-16 23:25:01 -0500

    Se agrega Log

--- src/main/java/com/tesji/estoico/AlumnoClient.java
+++ src/main/java/com/tesji/estoico/AlumnoClient.java
@@ -1,23 +1,26 @@
 package com.tesji.estoico;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.GenericType;
 
 public class AlumnoClient {
 
+	private static Logger LOG = Logger.getLogger(AlumnoClient.class.getName());
+	
 	public static void main(String[] args) {
 
 		List<Alumno> list = Client.create().resource("http://localhost:8080/HelloJersey/rest/alumnos/json")
 				.get(new GenericType<List<Alumno>>() {});
 
 		for (Alumno alumno : list) {
-			System.out.println(" ============ ");
-			System.out.println("Num Cuenta: " + alumno.getNumeroCuenta());
-			System.out.println("Nombre: " + alumno.getNombre());
-			System.out.println("Apellidos: " + alumno.getApellidos());
-			System.out.println("Carrera: " + alumno.getCarrera());
+			LOG.info(" ============ ");
+			LOG.info("Num Cuenta: " + alumno.getNumeroCuenta());
+			LOG.info("Nombre: " + alumno.getNombre());
+			LOG.info("Apellidos: " + alumno.getApellidos());
+			LOG.info("Carrera: " + alumno.getCarrera());
 		}
 
 	}
